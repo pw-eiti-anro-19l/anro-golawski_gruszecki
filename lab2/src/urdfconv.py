74d7412322d156813111167c2ddba80c0c28a5ac
deikare <35132895+deikare@users.noreply.github.com>
2019-05-06 08:07:32 +0200

    poprawka konwertera

--- lab2/src/urdfconv.py
+++ lab2/src/urdfconv.py
@@ -13,23 +13,26 @@ if __name__ == '__main__':
         params=json.loads(file.read())
 
     with open('../urdf.yaml', 'w') as file:
-        for key in params.keys():
-            a, d, al, th = params[key]
-            a, d, al, th = map(float, (a, d, al, th))
+        for param in params:
+            inst = json.loads(json.dumps(param))
+            a = inst["a"]
+	    d = inst["d"]
+	    al = inst["alpha"]
+	    th = inst["theta"]
 
             trans_z = translation_matrix((0,0,d))
             rot_z = rotation_matrix(th, zaxis)
             trans_x = translation_matrix((a,0,0))
             rot_x = rotation_matrix(al, xaxis)
 
-            mat = concatenate_matrices(trans_z, rot_z, trans_x, rot_x)
+            mat = concatenate_matrices(trans_x, rot_x, rot_z, trans_z)
 
             (roll, pitch, yaw) = euler_from_matrix(mat)
             (x,y,z) = translation_from_matrix(mat)
 
-            file.write(key + ":\n")
-            file.write("    joint_xyz: {} {} {}\n".format(x,y,z))
-            file.write("    joint_rpy: {} {} {}\n".format(roll, pitch, yaw))
-            file.write("    link_xyz: {} 0 0\n".format(x/2))
-            file.write("    link_rpy: 0 0 0\n")
-            file.write("    link_l: {}\n".format(a))
+            file.write(inst["name"] + ":\n")
+            file.write("    j_xyz: " + str(x) + " " + str(y) + " "+ str(z) + "\n")
+            file.write("    j_rpy: " + str(roll) + ' '+str(pitch) + ' ' + str(yaw) + "\n")
+            file.write("    l_xyz: " + str(0) + ' ' + str(0) + ' ' + str(float(d)*(-0.5)) + "\n")
+            file.write("    l_rpy: " + str(0) + ' ' + str(0) + ' ' + str(0) + "\n")
+            file.write("    l_len: " + str(d) + "\n")
